e22506ee36d1eedfc53c6e135526680c2b5cfb97
jchen788 <jchen788@gatech.edu>
2022-06-19 01:38:42 -0500

    changed packages in setup.py which fixes imports

--- src/a1_slam/setup.py
+++ src/a1_slam/setup.py
@@ -1,12 +1,13 @@
-from setuptools import setup
+from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
     packages=[
-        'a1_slam.src.nodes',
-        'a1_slam.src.sensors',
-        'a1_slam.src.registration',
-        'a1_slam.src.utils',
+        'a1_slam',
+        'nodes',
+        'sensors',
+        'registration',
+        'utils',
     ],
     package_dir={'': 'src'}
 )
