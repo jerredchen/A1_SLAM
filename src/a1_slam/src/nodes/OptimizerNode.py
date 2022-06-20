fcfd05fba8d020e632c9aba752f324edb38023de
jchen788 <jchen788@gatech.edu>
2022-06-20 14:09:36 -0500

    added tests for created poses and trajectory

--- src/a1_slam/src/nodes/OptimizerNode.py
+++ src/a1_slam/src/nodes/OptimizerNode.py
@@ -11,6 +11,7 @@ from unitree_legged_msgs.msg import HighState
 from a1_slam.srv import GtsamResults, FinalResults
 from gtsam.symbol_shorthand import B, V, X
 from geometry_msgs.msg import PoseStamped
+from nav_msgs.msg import Path
 from sensor_msgs.msg import LaserScan, PointCloud2
 
 
@@ -19,7 +20,11 @@ class OptimizerNode():
 
         # Instantiate publisher attributes.
         self.pose_publisher = rospy.Publisher(
-            "pose_estimate", PoseStamped, queue_size=5)
+            "/pose_estimate", PoseStamped, queue_size=5
+        )
+        self.traj_publisher = rospy.Publisher(
+            "/traj_estimate", Path, queue_size=5
+        )
 
         # Instantiate factor graph and optimizer attributes.
         self.graph = gtsam.NonlinearFactorGraph()
@@ -27,12 +32,12 @@ class OptimizerNode():
         self.results = gtsam.Values()
         self.isam = gtsam.ISAM2(gtsam.ISAM2Params())
 
-    def send_final_results(self, request):
+    def send_and_clear_results_callback(self, request):
         serialized_str = self.results.serialize()
         self.results.clear()
         return serialized_str
 
-    def optimize_graph(self, request):
+    def optimize_graph_callback(self, request):
         rospy.loginfo("optimizing factor graph")
 
         factor_type = request.factor_type
@@ -46,15 +51,26 @@ class OptimizerNode():
             factor = gtsam.BetweenFactorPose2(
                 0, 0, gtsam.Pose2(), gtsam.noiseModel.Isotropic.Sigma(3, 1))
             init_estimate = gtsam.Pose2()
+        elif factor_type == "PriorFactorPose3":
+            factor = gtsam.PriorFactorPose3(
+                0, gtsam.Pose3(), gtsam.noiseModel.Isotropic.Sigma(6, 1))
+            init_estimate = gtsam.Pose2()
+        elif factor_type == "BetweenFactorPose3":
+            factor = gtsam.BetweenFactorPose3(
+                0, 0, gtsam.Pose3(), gtsam.noiseModel.Isotropic.Sigma(6, 1))
+            init_estimate = gtsam.Pose3()
         else:
             rospy.logerr(
-                "Not currently supported for other than PriorFactorPose2")
+                "Not currently supported for other than Pose2 or Pose3")
         factor.deserialize(request.factor)
         self.graph.add(factor)
 
+        # Obtain the keys associated with the factor.
+        key_vector = factor.keys()
         if len(request.init_estimate) != 0:
             init_estimate.deserialize(request.init_estimate)
-            self.initial_estimates.insert(request.key, init_estimate)
+            
+            self.initial_estimates.insert(key_vector[-1], init_estimate)
 
         prev_results_size = self.results.size()
 
@@ -66,7 +82,8 @@ class OptimizerNode():
         if self.results.size() > prev_results_size and (
             "Pose2" in factor_type or "Pose3" in factor_type
         ):
-            self.publish_pose(request.key)
+            pose_msg = self.process_pose_message(key_vector[-1])
+            self.publish_pose(pose_msg)
 
         # Clear the graph and initial estimates, and update the state index.
         self.graph = gtsam.NonlinearFactorGraph()
@@ -74,9 +91,22 @@ class OptimizerNode():
 
         # Serialize the results to return.
         serialized_str = self.results.serialize()
+
         return serialized_str
 
-    def publish_pose(self, key):
+    def create_trajectory_callback(self, event=None):
+        trajectory = []
+        if rospy.get_param("/use_2dlidar"):
+            poses = gtsam.utilities.allPose2s(self.results)
+        else:
+            poses = gtsam.utilities.allPose3s(self.results)
+        keys = gtsam.KeyVector(poses.keys())
+        for key in keys:
+            trajectory.append(self.process_pose_message(key))
+        self.publish_traj(trajectory)
+
+    def process_pose_message(self, key):
+        """Publish a PoseStamped message of the most recent pose."""
         pose_msg = PoseStamped()
         pose_msg.header.stamp = rospy.Time.now()
         pose_msg.header.frame_id = "world"
@@ -92,22 +122,35 @@ class OptimizerNode():
             pose_msg.pose.position.y = pose_estimate.y()
             pose_msg.pose.position.y = pose_estimate.z()
             quaternion = gtsam.rotation().quaternion()
+        pose_msg.pose.orientation.w = quaternion[0]
         pose_msg.pose.orientation.x = quaternion[1]
         pose_msg.pose.orientation.y = quaternion[2]
         pose_msg.pose.orientation.z = quaternion[3]
-        pose_msg.pose.orientation.w = quaternion[0]
+        return pose_msg
+    
+    def publish_pose(self, pose_msg):
         self.pose_publisher.publish(pose_msg)
+        rospy.loginfo(f"publishing {pose_msg.header.seq=}")
+
+    def publish_traj(self, trajectory):
+        """Publish a Path message of the robot's trajectory."""
+        path_msg = Path()
+        path_msg.header.stamp = rospy.Time.now()
+        path_msg.header.frame_id = "world"
+        path_msg.poses = trajectory
+        self.traj_publisher.publish(path_msg)
 
     def launch_optimizer_node(self):
-        """
-        1. Instantiate the ros node
-        2. optimize_graph(graph, init_estimates, isam, pim=None, ...)
-        """
+        """Initialize and start the optimizer node."""
         rospy.init_node('optimizer_node', anonymous=True)
 
-        rospy.Service('optimizer_service', GtsamResults, self.optimize_graph)
-        # rospy.Service('final_results_service',
-        #               FinalResults, self.send_final_results)
+        # Start the necessary services for optimizing and sending results.
+        rospy.Service('optimizer_service', GtsamResults, self.optimize_graph_callback)
+        rospy.Service('final_results_service',
+                      FinalResults, self.send_and_clear_results_callback)
+
+        # Start a thread for publishing the trajectory.
+        rospy.Timer(rospy.Duration(0.5), self.create_trajectory_callback)
 
         # Instantiate the iSAM2 parameters to create the iSAM2 object.
         parameters = gtsam.ISAM2Params()
--- src/a1_slam/test/test_PoseSLAM.py
+++ src/a1_slam/test/test_PoseSLAM.py
@@ -1,49 +1,101 @@
 #!/usr/bin/env python
 
-import a1_slam
 import gtsam
 import numpy as np
 import rospy
 from unitree_legged_msgs.msg import HighState
-from a1_slam.srv import GtsamResults, FinalResults
+from a1_slam.srv import FinalResults
 from gtsam.symbol_shorthand import B, V, X
 from gtsam.utils.test_case import GtsamTestCase
-from geometry_msgs.msg import PoseStamped, Pose, Pose2D
+from geometry_msgs.msg import PoseStamped
 from nav_msgs.msg import Path
 from sensor_msgs.msg import LaserScan, PointCloud2
-from std_msgs.msg import String
 
 
 class TestPoseSLAM(GtsamTestCase):
     def setUp(self):
         rospy.init_node('test_node', anonymous=True)
         rospy.loginfo("Initialized test_node")
+        
+        # Instantiate toy data publishers.
         imu_topic = rospy.get_param('/imu/topic')
         lidar2d_topic = rospy.get_param('/lidar2d/topic')
         self.imu_pub = rospy.Publisher(imu_topic, HighState)
         self.lidar2d_pub = rospy.Publisher(lidar2d_topic, LaserScan)
+
+        # Instantiate relevant subscribers, attributes, and service to test results.
+        rospy.Subscriber('/pose_estimate', PoseStamped, self.poses_callback)
+        rospy.Subscriber('/traj_estimate', Path, self.traj_callback)
         rospy.wait_for_service('final_results_service')
-        self.send_results = rospy.ServiceProxy('final_results_service', FinalResults)
-        rospy.loginfo(f"{self.lidar2d_pub.get_num_connections()}")
+        self.send_and_clear_results = rospy.ServiceProxy(
+            'final_results_service', FinalResults)
+        self.poses = gtsam.Values()
+        self.traj = gtsam.Values()
+
         self.ranges_original = np.array(
-              [np.inf]*4
+            [np.inf]*4
             + [2, 2/np.sqrt(3), 1, 2/np.sqrt(3), 2]
             + [np.inf]*3
         )
+        # Sleep to allow for subscribers to connect to the created topics.
         rospy.sleep(2)
-        rospy.loginfo(f"{self.lidar2d_pub.get_num_connections()}")
+    
+    ############ Publisher/subscriber relevant functions ############
 
     def publish_msgs(self, publisher, msgs, rate):
+        """Publish toy data for testing.
+        Args:
+            publisher: a rospy.Publisher object to publish.
+            msgs: A generator of messages to be published.
+            rate: The rate to publish the messages, in Hz.
+        """
         ros_rate = rospy.Rate(rate)
         for msg in msgs:
             publisher.publish(msg)
             rospy.loginfo(f"publishing {msg.header.seq=}")
             ros_rate.sleep()
+    
+    def process_pose_msg(self, msg: PoseStamped):
+        """Convert PoseStamped message into GTSAM pose."""
+        rotation = gtsam.Rot3.Quaternion(
+            msg.pose.orientation.w,
+            msg.pose.orientation.x,
+            msg.pose.orientation.y,
+            msg.pose.orientation.z,
+        )
+        if rospy.get_param('/use_2dlidar'):
+            pose = gtsam.Pose2(
+                msg.pose.position.x,
+                msg.pose.position.y,
+                rotation.yaw()
+            )
+        else:
+            translation = gtsam.Point3(
+                msg.pose.position.x,
+                msg.pose.position.y,
+                msg.pose.position.z,
+            )
+            pose = gtsam.Pose3(rotation, translation)
+        return pose
+
+    def poses_callback(self, msg: PoseStamped):
+        pose = self.process_pose_msg(msg)
+        self.poses.insert(X(self.poses.size()), pose)
+    
+    def traj_callback(self, msg: Path):
+        new_traj = gtsam.Values()
+        for pose_msg in msg.poses:
+            pose = self.process_pose_msg(pose_msg)
+            new_traj.insert(X(new_traj.size()), pose)
+        self.traj = new_traj
+
+    ##################### Test data generators ######################
 
     def generate_stationary_imu_data(self):
         """Generate IMU measurements when stationary."""
         for _ in range(500):
             highstate = HighState()
+            highstate.header.stamp = rospy.Time.now()
             highstate.imu.accelerometer = [0, 0, 9.81]
             highstate.imu.gyroscope = [0, 0, 0]
             yield highstate
@@ -52,6 +104,7 @@ class TestPoseSLAM(GtsamTestCase):
         """Generate IMU measurements when accelerating."""
         for _ in range(500):
             highstate = HighState()
+            highstate.header.stamp = rospy.Time.now()
             highstate.imu.accelerometer = [1, 0, 9.81]
             highstate.imu.gyroscope = [0, 0, 0]
             yield highstate
@@ -60,13 +113,15 @@ class TestPoseSLAM(GtsamTestCase):
         """Generate LIDAR measurements when moving."""
         for i in range(5):
             scan = LaserScan()
+            scan.header.stamp = rospy.Time.now()
             scan.angle_increment = -np.pi/6
             scan.angle_min = np.pi
             scan.angle_max = -np.pi
             ranges = ((1.0 - 0.1*i) * self.ranges_original).tolist()
-            rospy.loginfo(f"{ranges=}")
             scan.ranges = ranges
             yield scan
+
+    ####################### Integration tests #######################
 
     # def test_stationary_imu_poses(self):
     #     self.publish_msgs(self.imu_pub, self.generate_stationary_imu_data(), 100)
@@ -102,21 +157,26 @@ class TestPoseSLAM(GtsamTestCase):
 
     def test_lidar_poses(self):
         self.publish_msgs(
-            self.lidar2d_pub,
-            self.generate_lidar_data(),
-            10
-        )
+            self.lidar2d_pub, self.generate_lidar_data(), 10)
 
         # Generate the expected LIDAR poses.
         expected = gtsam.Values()
         for i in range(5):
             expected.insert(X(i), gtsam.Pose2(0.1*i, 0, 0))
 
+        # Sleep to finish obtaining the trajectory before clearing.
+        rospy.sleep(1)
+
         # Obtain the actual poses.
-        response = self.send_results()
+        response = self.send_and_clear_results()
         actual = gtsam.Values()
         actual.deserialize(response.results)
+
         self.gtsamAssertEquals(actual, expected, 1e-2)
+        self.gtsamAssertEquals(self.poses, expected, 1e-2)
+        self.gtsamAssertEquals(self.traj, expected, 1e-2)
+        self.poses.clear()
+        self.traj.clear()
 
 
 if __name__ == '__main__':
