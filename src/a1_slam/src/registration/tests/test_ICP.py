2ddb11f19f7e62e016ef45857cf3b1efd52c8cb2
jchen788 <jchen788@gatech.edu>
2022-06-22 16:13:52 -0500

    updated vanilla icp using gtsam 4.2.0 functions

--- src/a1_slam/src/registration/tests/test_ICP.py
+++ src/a1_slam/src/registration/tests/test_ICP.py
@@ -8,53 +8,45 @@ import numpy as np
 
 import gtsam
 from gtsam.utils.test_case import GtsamTestCase
-import vanilla_ICP
+from registration import vanilla_ICP
 
 class TestICP(GtsamTestCase):
 
   def setUp(self):
-    self.scan_a = np.array([[0, 0, 1], [0, 1, 0]])
-    self.scan_b = np.array([[1, 1, 2], [0, 1, 0]])
-    self.scan_c = np.array([[1], [1]])
-    self.scan_d = np.array([[2], [1]])
-    self.scan_f = np.array([[0, 20, 10], [0, 10, 20]])
-    self.scan_g = np.array([[10, 30, 20], [10, 20, 30]])
-    self.scan_h = np.array([[0.0, 1.0, 1.0], [0.0, 0.0, 0.5]])
-    self.scan_i = np.array([[0, -1, 0], [0, 0, 1]])
-    self.scan_j = np.array([[0, 0, -1], [0, -1, 0]])
+    self.scan_a = np.array([[0, 0, 1], [0, 1, 0], [1, 1, 1,]])
+    self.scan_b = np.array([[1, 1, 2], [0, 1, 0], [1, 1, 1,]])
+    self.scan_c = np.array([[1], [1], [1]])
+    self.scan_d = np.array([[2], [1], [1]])
+    self.scan_f = np.array([[0, 20, 10], [0, 10, 20], [1, 1, 1,]])
+    self.scan_g = np.array([[10, 30, 20], [10, 20, 30], [1, 1, 1,]])
+    self.scan_h = np.array([[0.0, 1.0, 1.0], [0.0, 0.0, 0.5], [1, 1, 1,]])
+    self.scan_i = np.array([[0, -1, 0], [0, 0, 1], [1, 1, 1,]])
+    self.scan_j = np.array([[0, 0, -1], [0, -1, 0], [1, 1, 1,]])
 
   def test_transform_scan_1(self):
     actual_scan = vanilla_ICP.transform_scan(self.scan_a, gtsam.Pose2(1,0,0))
     np.testing.assert_allclose(actual_scan, self.scan_b)
-  
-  def test_transform_scan_2(self):
-    actual_scan = vanilla_ICP.transform_scan(self.scan_c, gtsam.Pose2(1,0,0))
-    np.testing.assert_allclose(actual_scan, self.scan_d)
 
-  def test_transform_scan_3(self):
+  def test_transform_scan_2(self):
     actual_scan = vanilla_ICP.transform_scan(self.scan_f, gtsam.Pose2(10,10,0))
     np.testing.assert_allclose(actual_scan, self.scan_g)
 
   def test_estimate_transform_SVD_1(self):
-    actual_transform = vanilla_ICP.estimate_transform_SVD(self.scan_a, self.scan_b)
-    self.gtsamAssertEquals(actual_transform, gtsam.Pose2(1,0,0), tol=1e-6)
-
-  def test_estimate_transform_SVD_2(self):
-    actual_transform = vanilla_ICP.estimate_transform_SVD(self.scan_c, self.scan_d)
+    actual_transform = vanilla_ICP.estimate_transform(self.scan_a, self.scan_b)
     self.gtsamAssertEquals(actual_transform, gtsam.Pose2(1,0,0), tol=1e-6)
 
   def test_estimate_transform_SVD_3(self):
-    actual_transform = vanilla_ICP.estimate_transform_SVD(self.scan_f, self.scan_g)
+    actual_transform = vanilla_ICP.estimate_transform(self.scan_f, self.scan_g)
     self.gtsamAssertEquals(actual_transform, gtsam.Pose2(10,10,0), tol=1e-6)
   
   def test_estimate_transform_SVD_4(self):
     expected_transform = gtsam.Pose2(0, 0, np.pi/2)
-    actual_transform = vanilla_ICP.estimate_transform_SVD(self.scan_a, self.scan_i)
+    actual_transform = vanilla_ICP.estimate_transform(self.scan_a, self.scan_i)
     self.gtsamAssertEquals(actual_transform, expected_transform, tol=1e-6)
 
   def test_estimate_transform_SVD_5(self):
     expected_transform = gtsam.Pose2(0, 0, np.pi)
-    actual_transform = vanilla_ICP.estimate_transform_SVD(self.scan_a, self.scan_j)
+    actual_transform = vanilla_ICP.estimate_transform(self.scan_a, self.scan_j)
     self.gtsamAssertEquals(actual_transform, expected_transform, tol=1e-6)
   
   def test_icp_1(self):
@@ -65,16 +57,8 @@ class TestICP(GtsamTestCase):
     expected_transform = gtsam.Pose2(-1, 0, 0)
     actual_transform = vanilla_ICP.icp(self.scan_b, self.scan_a)
     np.testing.assert_almost_equal(actual_transform.x(), expected_transform.x(), decimal=1)
-
-  def test_icp_2(self):
-    """
-    Point shifts one unit to the right.
-    """
-    expected_transform = gtsam.Pose2(1, 0, 0)
-    actual_transform = vanilla_ICP.icp(self.scan_c, self.scan_d)
-    self.gtsamAssertEquals(actual_transform, expected_transform, tol=1e-6)
   
-  def test_icp_3(self):
+  def test_icp_2(self):
     """
     Vertical line shift 1 units to the right.
     """
@@ -84,7 +68,7 @@ class TestICP(GtsamTestCase):
     actual_transform = vanilla_ICP.icp(scan_a, scan_b)
     self.gtsamAssertEquals(actual_transform, aTb, tol=1e-1)
 
-  def test_icp_4(self):
+  def test_icp_3(self):
     """
     Triangle shifts one unit to the left and downward.
     """
@@ -92,7 +76,7 @@ class TestICP(GtsamTestCase):
     actual_transform = vanilla_ICP.icp(self.scan_h + 1.0, self.scan_h)
     self.gtsamAssertEquals(actual_transform, expected_transform, tol=1e-1)
   
-  def test_icp_5(self):
+  def test_icp_4(self):
     """
     Vertical line shifts 0.1 unit down.
     """
@@ -102,7 +86,7 @@ class TestICP(GtsamTestCase):
     actual_transform = vanilla_ICP.icp(scan_a, scan_b)
     self.gtsamAssertEquals(actual_transform, aTb, tol=1e-2)
   
-  def test_icp_6(self):
+  def test_icp_5(self):
     """
     Horizontal line shifts 0.4 unit left.
     """
--- src/a1_slam/src/registration/vanilla_ICP.py
+++ src/a1_slam/src/registration/vanilla_ICP.py
@@ -10,40 +10,39 @@ import gtsam
 
 def transform_scan(scan_a, transform):
     """Transform a given point cloud using a specified transformation."""
-    return (transform.matrix() @ scan_a)
+    # return (transform.matrix() @ scan_a)
+    return np.vstack((transform.transformFrom(scan_a[:2]), np.ones((1,scan_a.shape[1]))))
 
 def align_nearest_points(scan_a, scan_b):
     """Align the points in scan_a with its nearest neighbor points in scan_b."""
-    scan_bT = scan_b[:].T
+    scan_bT = scan_b[:2][:].T
     neighbors = NearestNeighbors(n_neighbors=1, radius=0.05).fit(scan_bT)
-    ind = (neighbors.kneighbors(scan_a.T, return_distance=False).T)[0]
-    return (scan_bT[ind]).T
+    ind = (neighbors.kneighbors(scan_a[:2].T, return_distance=False).T)[0]
+    return ((scan_b.T)[ind]).T
 
-def estimate_transform_SVD(scan_a, scan_b):
+def estimate_transform(scan_a, scan_b):
     """Estimate the transform between the two point clouds using SVD."""
-    centroid_a = np.expand_dims(np.average(scan_a, axis=1), 1)
-    centroid_b = np.expand_dims(np.average(scan_b, axis=1), 1)
-    scan_a_prime = scan_a - centroid_a
-    scan_b_prime = scan_b - centroid_b
-    H = np.sum(scan_a_prime.T[:,:,None]*scan_b_prime.T[:,None], axis=0)
-    u, _, vh = np.linalg.svd(H)
-    R_mat = vh.T @ u.T
-    if np.isclose(np.linalg.det(R_mat), -1):
-        v = vh.T
-        R_mat = np.hstack((v[:,0,None], v[:,1,None], -v[:,2,None]))
-    theta = np.arctan2(R_mat[1,0], R_mat[0,0])
-    t_vec = centroid_b - (R_mat @ centroid_a)
-    return gtsam.Pose2(theta, t_vec[:2])
+    # centroid_a = np.expand_dims(np.average(scan_a, axis=1), 1)
+    # centroid_b = np.expand_dims(np.average(scan_b, axis=1), 1)
+    # scan_a_prime = scan_a - centroid_a
+    # scan_b_prime = scan_b - centroid_b
+    # H = np.sum(scan_a_prime.T[:,:,None]*scan_b_prime.T[:,None], axis=0)
+    # u, _, vh = np.linalg.svd(H)
+    # R_mat = vh.T @ u.T
+    # if np.isclose(np.linalg.det(R_mat), -1):
+    #     v = vh.T
+    #     R_mat = np.hstack((v[:,0,None], v[:,1,None], -v[:,2,None]))
+    # theta = np.arctan2(R_mat[1,0], R_mat[0,0])
+    # t_vec = centroid_b - (R_mat @ centroid_a)
+    # return gtsam.Pose2(theta, t_vec[:2])
+    return gtsam.Pose2.Align(scan_b[:2], scan_a[:2])
 
 def icp(scan_a, scan_b, initial_transform=gtsam.Pose2(), max_iterations=25, plotPoints=False):
     """Perform standard vanilla ICP between two 2D LiDAR scans."""
-    delta = float('inf')
-    threshold = 0.01
     transform = initial_transform
     icp_transform = initial_transform
 
-    i = 0
-    while delta > threshold and i < max_iterations:
+    for _ in range(max_iterations):
         # Transform the scan using the previously used transform.
         scan_a = transform_scan(scan_a, transform)
 
@@ -62,14 +61,13 @@ def icp(scan_a, scan_b, initial_transform=gtsam.Pose2(), max_iterations=25, plot
             plt.pause(0.5)
 
         # Estimate the transform between the aligned scan_b points and the transformed scan_a points.
-        estimated_transform = estimate_transform_SVD(scan_a, scan_c)
+        estimated_transform = estimate_transform(scan_a, scan_c)
 
         # Check to see if the estimated transform changed from its previous iteration.
-        delta = np.linalg.norm(scan_c - scan_a)
+        if transform.equals(estimated_transform, tol=1e-2): break
 
         # Reinitialize the transforms for the next iteration.
         icp_transform = icp_transform.compose(estimated_transform)
         transform = estimated_transform
-        i += 1
 
     return icp_transform
