87f42828dc5f91865288d3c28ae0135c69ae7cbb
jchen788 <jchen788@gatech.edu>
2022-07-04 16:43:05 -0500

    added imu callback and services

--- src/a1_slam/src/nodes/ImuNode.py
+++ src/a1_slam/src/nodes/ImuNode.py
@@ -7,7 +7,7 @@ import rospy
 
 import gtsam
 import numpy as np
-from a1_slam.srv import GtsamResults
+from a1_slam.srv import GtsamResults, ImuEstimate
 from gtsam.symbol_shorthand import B, V, X
 from unitree_legged_msgs.msg import HighState
 
@@ -17,6 +17,10 @@ class ImuNode():
     def __init__(self):
         self.request_optimizer = None
 
+        self.results = gtsam.Values()
+
+        self.timestamp = 0
+        self.seen_measurements = 0
         self.pim = None
         self.state_index = 0
 
@@ -116,7 +120,38 @@ class ImuNode():
             pim_parameters, bias)
 
     def imu_callback(self, msg: HighState):
-        pass
+        if self.timestamp == 0:
+            self.timestamp = msg.header.stamp.to_sec()
+            self.seen_measurements += 1
+            return
+        current_timestamp = msg.header.stamp.to_sec()
+        self.preintegrate_measurement(msg, current_timestamp - self.timestamp)
+        self.timestamp = current_timestamp
+        self.seen_measurements += 1
+        use_2dlidar = rospy.get_param('/use_2dlidar')
+        use_depth = rospy.get_param('/use_depth')
+        factor_rate = rospy.get_param('/imu/imu_factor_rate')
+        if (not use_2dlidar and not use_depth and \
+            self.seen_measurements == factor_rate):
+                imu_factor, predicted_navstate = self.add_IMU_factor(
+                    self.state_index - 1,
+                    self.state_index
+                )
+                serialized_factor = imu_factor.serialize()
+                serialized_estimate = predicted_navstate.serialize()
+                response = self.request_optimizer(
+                    "ImuFactor",
+                    serialized_factor,
+                    serialized_estimate,
+                    X(self.state_index),
+                    V(self.state_index),
+                    -1
+                )
+                received_results = gtsam.Values()
+                received_results.deserialize(response.results)
+                self.results = received_results
+                self.state_index += 1
+                self.seen_measurements = 0
 
     def preintegrate_measurement(self,
                                  high_state_msg: HighState,
@@ -143,20 +178,50 @@ class ImuNode():
             navstate:   The navigation state used to predict the next state.
         Returns:
             factor: An IMU factor to be added to the factor graph.
-            predicted_state: The navigation state used as an initial estimate.
+            predicted_navstate: The navigation state used as an initial estimate.
         """
         # Add the IMU factor to the factor graph.
         imu_factor = gtsam.ImuFactor(
             X(i), V(i), X(j), V(j), B(0), self.pim)
 
         # Initialize the estimates for the pose and velocity by integrating from the previous state.
-        predicted_state = self.pim.predict(
+        predicted_navstate = self.pim.predict(
             navstate, self.results.atConstantBias(B(0)))
 
         # Reset the integration of the pim.
         self.pim.resetIntegration()
 
-        return imu_factor, predicted_state
+        return imu_factor, predicted_navstate
+    
+    def send_imu_estimate(self, request):
+        """Send the preintegrated IMU estimate to use as an initial estimate
+        for LIDAR odometry.
+        """
+        i, j = request.current_index, request.predicted_index
+        current_navstate = gtsam.NavState(
+            self.results.atPose3(X(i)),
+            self.results.atVector(V(i)),
+        )
+        imu_factor, predicted_navstate = self.add_IMU_factor(
+            i,
+            j,
+            current_navstate
+        )
+        serialized_factor = imu_factor.serialize()
+        serialized_estimate = predicted_navstate.serialize()
+        response = self.request_optimizer(
+            "ImuFactor",
+            serialized_factor,
+            serialized_estimate,
+            X(j),
+            V(j),
+            -1
+        )
+        received_results = gtsam.Values()
+        received_results.deserialize(response.results)
+        self.results = received_results
+        self.state_index += 1
+        return predicted_navstate.pose().serialize()
 
     def launch_imu_node(self):
 
@@ -167,28 +232,32 @@ class ImuNode():
         self.request_optimizer = rospy.ServiceProxy(
             'optimizer_service', GtsamResults)
 
+        # Start the necessary services for sending predicted NavStates.
+        rospy.Service('initial_estimate_service', ImuEstimate,
+                      self.send_imu_estimate)
+
         # Obtain the pose's translational and rotational standard deviations, in meters and degrees.
-        prior_pose_sigmas = rospy.get_param('prior_pose_sigmas')
+        prior_pose_sigmas = rospy.get_param('/prior_pose_sigmas')
 
         # Obtain the velocity's standard deviation in all directions, in m/s.
-        prior_vel_sigmas = rospy.get_param('prior_vel_sigmas')
+        prior_vel_sigmas = rospy.get_param('/imu/prior_vel_sigmas')
 
         # Obtain the bias' standard deviation in all directions, in m/s^2 and rad/s.
-        prior_bias_sigmas = rospy.get_param('prior_bias_sigmas')
+        prior_bias_sigmas = rospy.get_param('/imu/prior_bias_sigmas')
 
         # Obtain the accelerometer noise standard deviations in all directions, in m/s^2.
-        accel_sigmas = rospy.get_param('accel_sigmas')
+        accel_sigmas = rospy.get_param('/imu/accel_sigmas')
 
         # Obtain the gyroscope noise standard deviations in all directions, in rad/s.
-        gyro_sigmas = rospy.get_param('gyro_sigmas')
+        gyro_sigmas = rospy.get_param('/imu/gyro_sigmas')
 
         # Obtain the integration noise standard deviations in all directions.
-        integration_sigmas = rospy.get_param('integration_sigmas')
+        integration_sigmas = rospy.get_param('/imu/integration_sigmas')
 
         # Obtain the initital estimates for the prior poses, velocities, and IMU biases.
-        prior_pose_estimate = rospy.get_param('prior_pose_estimate')
-        prior_vel_estimate = rospy.get_param('prior_vel_estimate')
-        prior_bias_estimate = rospy.get_param('prior_bias_estimate')
+        prior_pose_estimate = rospy.get_param('/prior_pose_estimate')
+        prior_vel_estimate = rospy.get_param('/imu/prior_vel_estimate')
+        prior_bias_estimate = rospy.get_param('/imu/prior_bias_estimate')
 
         # Parse the config parameters to be GTSAM appropriate objects.
         parsed_config_params = self.parse_config_parameters(
@@ -209,13 +278,37 @@ class ImuNode():
         gyro_noise_cov = parsed_config_params[4]
         integration_noise_cov = parsed_config_params[5]
 
-        # Request results from adding prior to the factor graph.
-        serialized_factor = prior_pose_factor.serialize()
-        serialized_estimate = prior_pose_factor.prior().serialize()
+        # Request results from adding priors to the factor graph.
+        serialized_prior_pose = prior_pose_factor.serialize()
+        serialized_pose_estimate = prior_pose_factor.prior().serialize()
+        serialized_prior_vel = prior_vel_factor.serialize()
+        velocity = prior_vel_factor.prior()
+        serialized_vel_estimate = f"{velocity[0]} {velocity[1]} {velocity[2]}"
+        serialized_prior_bias = prior_bias_factor.serialize()
+        serialized_bias_estimate = prior_bias_factor.prior().serialize()
         response = self.request_optimizer(
             "PriorFactorPose3",
-            serialized_factor,
-            serialized_estimate
+            serialized_prior_pose,
+            serialized_pose_estimate,
+            X(self.state_index),
+            -1,
+            -1
+        )
+        response = self.request_optimizer(
+            "PriorFactorVector",
+            serialized_prior_vel,
+            serialized_vel_estimate,
+            -1,
+            V(self.state_index),
+            -1
+        )
+        response = self.request_optimizer(
+            "PriorFactorConstantBias",
+            serialized_prior_bias,
+            serialized_bias_estimate,
+            -1,
+            -1,
+            B(0)
         )
         received_results = gtsam.Values()
         received_results.deserialize(response.results)
