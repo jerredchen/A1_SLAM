581c2b53a9a6885707404942e8a9e8f006719250
jchen788 <jchen788@gatech.edu>
2022-07-05 10:39:20 -0500

    added integration test with both lidar and imu

--- src/a1_slam/test/test_PoseSLAM.py
+++ src/a1_slam/test/test_PoseSLAM.py
@@ -4,7 +4,7 @@ import gtsam
 import numpy as np
 import rospy
 from unitree_legged_msgs.msg import HighState
-from a1_slam.srv import FinalResults, ClearResults
+from a1_slam.srv import ClearResults
 from gtsam.symbol_shorthand import B, V, X
 from gtsam.utils.test_case import GtsamTestCase
 from geometry_msgs.msg import PoseStamped
@@ -26,9 +26,6 @@ class TestPoseSLAM(GtsamTestCase):
         # Instantiate relevant subscribers, attributes, and service to test results.
         rospy.Subscriber('/pose_estimate', PoseStamped, self.poses_callback)
         rospy.Subscriber('/traj_estimate', Path, self.traj_callback)
-        rospy.wait_for_service('final_results_service')
-        self.send_results = rospy.ServiceProxy(
-            'final_results_service', FinalResults)
         rospy.wait_for_service('clear_results_service')
         self.clear_results = rospy.ServiceProxy(
             'clear_results_service', ClearResults)
@@ -55,7 +52,34 @@ class TestPoseSLAM(GtsamTestCase):
         ros_rate = rospy.Rate(rate)
         for msg in msgs:
             publisher.publish(msg)
-            rospy.loginfo(f"publishing {msg.header.seq=}")
+            ros_rate.sleep()
+    
+    def publish_multi_msgs(self, publishers, msgs, rates):
+        """Publish toy data of multiple simultaneous sensors.
+        Args:
+            publishers: A list of rospy.Publisher objects, sorted from highest
+                        publishing frequency to lowest publishing frequency.
+            msgs:       A generator of varying sensor messages to be published.
+            rates:      A list of rates for each sensor, sorted from highest
+                        publishing frequency to lowest publishing frequency.
+        """
+        # Separate the highest frequency rate and the remaining lower frequencies.
+        max_rate = rates[0]
+
+        # Calculate the number of high frequency measurements to be published before
+        # publishing a lower frequency measurement.
+        pass_measurements = max_rate / np.array(rates[1:])
+        ros_rate = rospy.Rate(rates[0])
+        for i, msg in enumerate(msgs):
+
+            # Always publish the sensor at the fastest rate.
+            publishers[0].publish(msg)
+
+            # Check to see if a lower frequency sensor should publish.
+            for j, pass_measurement in enumerate(pass_measurements):
+                if i % pass_measurement == 0:
+                    publishers[j + 1].publish(msg)
+                    break
             ros_rate.sleep()
     
     def process_pose_msg(self, msg: PoseStamped):
@@ -87,7 +111,7 @@ class TestPoseSLAM(GtsamTestCase):
 
     ##################### Test data generators ######################
 
-    def generate_stationary_imu_data(self):
+    def generate_no_acceleration_imu_data(self):
         """Generate IMU measurements when stationary."""
         for _ in range(500):
             highstate = HighState()
@@ -116,11 +140,31 @@ class TestPoseSLAM(GtsamTestCase):
             ranges = ((1.0 - 0.1*i) * self.ranges_original).tolist()
             scan.ranges = ranges
             yield scan
+    
+    def generate_lidar_imu_data(self):
+        """Generate LIDAR and IMU measurements when moving."""
+        for i in range(500):
+            highstate = HighState()
+            highstate.header.stamp = rospy.Time.now()
+            highstate.imu.accelerometer = [0, 0, 9.81]
+            highstate.imu.gyroscope = [0, 0, 0]
+            yield highstate
+            if i % 50 == 0:
+                scan = LaserScan()
+                scan.header.stamp = rospy.Time.now()
+                scan.angle_increment = -np.pi/6
+                scan.angle_min = np.pi
+                scan.angle_max = -np.pi
+                ranges = ((1.0 - 0.1*i) * self.ranges_original).tolist()
+                scan.ranges = ranges
+                yield scan
 
     ####################### Integration tests #######################
 
     def test_stationary_imu_poses(self):
-        self.publish_msgs(self.imu_pub, self.generate_stationary_imu_data(), 100)
+        rospy.set_param('/use_imu', True)
+        rospy.set_param('/use_2dlidar', False)
+        self.publish_msgs(self.imu_pub, self.generate_no_acceleration_imu_data(), 100)
 
         # Generate the expected pose, velocity, and bias values.
         expected = gtsam.Values()
@@ -133,19 +177,18 @@ class TestPoseSLAM(GtsamTestCase):
         rospy.sleep(1)
 
         # Obtain the actual pose, velocity, and bias values.
-        response = self.send_results()
-        is_cleared = self.clear_results()
-        if not is_cleared.response:
-            rospy.logerr("Results were not properly cleared.")
+        response = self.clear_results()
         actual = gtsam.Values()
-        actual.deserialize(response.str)
+        actual.deserialize(response.results)
 
         self.gtsamAssertEquals(actual, expected)
         self.gtsamAssertEquals(self.poses, expected, 1e-2)
         self.gtsamAssertEquals(self.traj, expected, 1e-2)
 
     def test_acceleration_imu_poses(self):
-        self.publish_msgs(self.imu_pub, self.generate_stationary_imu_data(), 100)
+        rospy.set_param('/use_imu', True)
+        rospy.set_param('/use_2dlidar', False)
+        self.publish_msgs(self.imu_pub, self.generate_acceleration_imu_data(), 100)
 
         # Generate the expected pose, velocity, and bias values.
         expected = gtsam.Values()
@@ -158,12 +201,9 @@ class TestPoseSLAM(GtsamTestCase):
         rospy.sleep(1)
 
         # Obtain the actual pose, velocity, and bias values.
-        response = self.send_results()
-        is_cleared = self.clear_results()
-        if not is_cleared.response:
-            rospy.logerr("Results were not properly cleared.")
+        response = self.clear_results()
         actual = gtsam.Values()
-        actual.deserialize(response.str)
+        actual.deserialize(response.results)
 
         self.gtsamAssertEquals(actual, expected, 1e-3)
         self.gtsamAssertEquals(self.poses, expected, 1e-3)
@@ -171,8 +211,9 @@ class TestPoseSLAM(GtsamTestCase):
         self.poses.clear()
         self.traj.clear()
 
-
     def test_lidar_poses(self):
+        rospy.set_param('/use_imu', False)
+        rospy.set_param('/use_2dlidar', True)
         self.publish_msgs(
             self.lidar2d_pub, self.generate_lidar_data(), 10)
 
@@ -185,10 +226,35 @@ class TestPoseSLAM(GtsamTestCase):
         rospy.sleep(1)
 
         # Obtain the actual poses.
-        response = self.send_results()
-        is_cleared = self.clear_results()
-        if not is_cleared.response:
-            rospy.logerr("Results were not properly cleared.")
+        response = self.clear_results()
+        actual = gtsam.Values()
+        actual.deserialize(response.results)
+
+        self.gtsamAssertEquals(actual, expected, 1e-2)
+        self.gtsamAssertEquals(self.poses, expected, 1e-2)
+        self.gtsamAssertEquals(self.traj, expected, 1e-2)
+        self.poses.clear()
+        self.traj.clear()
+    
+    def test_lidar_with_imu(self):
+        rospy.set_param('/use_imu', True)
+        rospy.set_param('/use_2dlidar', True)
+        self.publish_multi_msgs(
+            [self.imu_pub, self.lidar2d_pub],
+            self.generate_lidar_imu_data(),
+            [500, 10]
+        )
+
+        # Generate the expected LIDAR poses.
+        expected = gtsam.Values()
+        for i in range(10):
+            expected.insert(X(i), gtsam.Pose3(gtsam.Pose2(0.1*i, 0, 0)))
+
+        # Sleep to finish obtaining the trajectory before clearing.
+        rospy.sleep(1)
+
+        # Obtain the actual poses.
+        response = self.clear_results()
         actual = gtsam.Values()
         actual.deserialize(response.results)
 
