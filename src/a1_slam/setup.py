d03d75ba6ca9dca3b3636bcb0ce00105c9baeb0a
jchen788 <jchen788@gatech.edu>
2022-07-19 00:49:40 -0400

    updated build and configs

--- src/a1_slam/setup.py
+++ src/a1_slam/setup.py
@@ -3,9 +3,10 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
     packages=[
-        'a1_slam',
         'nodes',
-        'registration'
+        'registration',
+        'sensors',
+        'optimization'
     ],
     package_dir={'': 'src'}
 )
