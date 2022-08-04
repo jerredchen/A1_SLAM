c90aa6e5b170aa4b3e00678656c51861daae1cc3
jchen788 <jchen788@gatech.edu>
2022-08-04 18:28:55 -0400

    add 3d lidar option

--- src/a1_slam/src/sensors/lidar3D.py
+++ src/a1_slam/src/sensors/lidar3D.py
@@ -1,32 +1,29 @@
 #!/usr/bin/env python
 
 """
-Class for processing 2D LIDAR scans.
+Class for processing 3D LIDAR clouds.
 """
 
 from collections import deque
 from threading import Lock
-import time
 
 import gtsam
 import numpy as np
 import pygicp
 import rospy
 from gtsam.symbol_shorthand import X
+from optimization.optimizer import Optimizer
 from sensor_msgs import point_cloud2
-from sensor_msgs.msg import LaserScan, PointCloud2
-from sklearn.neighbors import NearestNeighbors
+from sensor_msgs.msg import PointCloud2
 from std_msgs.msg import Header
 
-from optimization.optimizer import Optimizer
 
-
-class Lidar2DWrapper:
+class Lidar3DWrapper:
 
     def __init__(self, optimizer: Optimizer):
 
         # Instantiate publisher attributes.
-        self.scan_publisher = rospy.Publisher(
+        self.cloud_publisher = rospy.Publisher(
             "/transformed_clouds", PointCloud2, queue_size=5
         )
 
@@ -34,43 +31,40 @@ class Lidar2DWrapper:
         self.state_index = 1
         self.optimizer = optimizer
 
-        # Instantiate 2D LIDAR related attributes.
-        self.submap_scans = []
+        # Instantiate 3D LIDAR related attributes.
+        self.submap_clouds = []
         self.correspondence_threshold = 1.0
         self.icp_noise_model = gtsam.noiseModel.Diagonal.Sigmas(np.ones((6,)))
 
         # Instantiate a threading lock.
-        self.lidar2d_lock = Lock()
+        self.lidar3d_lock = Lock()
 
-    def preprocess_measurement(self, laser_msg: LaserScan, min_range=0, max_range=np.inf):
-        """Extracts ranges from the LaserScan message and converts into
-        a 2*N homogenous matrix of points.
+    def preprocess_measurement(self, laser_msg: PointCloud2, min_range=0, max_range=np.inf):
+        """Extracts ranges from the Lasercloud message and converts into
+        a 3*N matrix of points.
 
         Args:
-            laser_msg: The LaserScan message from the A1.
+            laser_msg: The PointCloud2 message from the A1.
         Returns:
-            scan: A numpy array of shape (2, N), where N is the number of 2D points.
+            cloud: A numpy array of shape (3, N), where N is the number of 3D points.
         """
-        scan = np.zeros((2, len(laser_msg.ranges)), dtype=float)
-        # Compute the 2D point where the angle starts at +90 deg, and
-        # each range is spaced out in equal increments.
-        for i, distance in enumerate(laser_msg.ranges):
-            if min_range < distance < max_range:
-                scan[0][i] = distance * \
-                    np.cos(laser_msg.angle_min + i*laser_msg.angle_increment)
-                scan[1][i] = distance * \
-                    np.sin(laser_msg.angle_min + i*laser_msg.angle_increment)
-        # Remove all values in the array that are still 0.
-        mask = scan[0] != 0
-        scan = scan[:, mask]
+        point_vector_list = point_cloud2.read_points_list(laser_msg)
+        points = [[point_vec.x, point_vec.y, point_vec.z]
+                  for point_vec in point_vector_list
+                  if min_range <= np.linalg.norm([
+                      point_vec.x(),
+                      point_vec.y(),
+                      point_vec.z()
+                  ]) <= max_range]
+        point_cloud = np.array(points).T
 
-        return scan
+        return point_cloud
 
     def create_lidar_factor(self,
                             a: int,
                             b: int,
-                            scan_a: np.ndarray,
-                            scan_b: np.ndarray,
+                            cloud_a: np.ndarray,
+                            cloud_b: np.ndarray,
                             aTb_estimate=None):
         """Creates an odometry factor from LIDAR measurements to be
         added to the factor graph.
@@ -78,8 +72,8 @@ class Lidar2DWrapper:
         Args:
             a: The previous state index of the odometry factor.
             b: The subsequent state index of the odometry factor.
-            scan_a: The LIDAR scan associated with state a.
-            scan_b: The LIDAR scan associated with state b.
+            cloud_a: The LIDAR cloud associated with state a.
+            cloud_b: The LIDAR cloud associated with state b.
             aTb_estimate: Initial guess for ICP.
         Returns:
             factor: An odometry factor to be added to the factor graph.
@@ -100,21 +94,17 @@ class Lidar2DWrapper:
                 aTb_estimate = wTp.between(wTq)
 
         # Use multithreaded GICP to calculate the rigid body transform.
-        source = np.vstack((scan_b, np.ones(scan_b.shape[1])))
-        target = np.vstack((scan_a, np.ones(scan_a.shape[1])))
         aTb_matrix = pygicp.align_points(
-            target.T,
-            source.T,
+            cloud_a.T,
+            cloud_b.T,
             max_correspondence_distance=self.correspondence_threshold,
             initial_guess=aTb_estimate.matrix(),
             k_correspondences=15,
             num_threads=2
         )
 
-        # Set z-translation to be 0 and convert back into Pose3 object.
+        # Convert back into Pose3 object.
         aTb = gtsam.Pose3(aTb_matrix)
-        rotation = aTb.rotation().Ypr(aTb.rotation().yaw(), 0, 0)
-        aTb = gtsam.Pose3(rotation, np.array([aTb.x(), aTb.y(), 0.0]))
         factor = gtsam.BetweenFactorPose3(
             X(a), X(b), aTb, self.icp_noise_model)
 
@@ -124,11 +114,11 @@ class Lidar2DWrapper:
 
         return factor, wTb_estimate
 
-    def lidar_callback(self, msg: LaserScan, imu=None):
+    def lidar_callback(self, msg: PointCloud2, imu=None):
         """Processes the message from the LIDAR and create a IMU factor.
 
         Args:
-            msg: a LaserScan ROS message.
+            msg: a PointCloud2 ROS message.
             optimizer: an Optimizer object to add the factors and initial estimates.
             imu: if not None, an Imu object to use as an initial estimate for ICP.
         """
@@ -136,47 +126,47 @@ class Lidar2DWrapper:
         # Calculate the aTb initial estimate from IMU if available.
         aTb_estimate = None
         index_a, index_b = self.state_index - 1, self.state_index
-        if imu and len(self.submap_scans) > 0:
+        if imu and len(self.submap_clouds) > 0:
             aTb_estimate = imu.create_and_add_factor(index_a, index_b)
 
         # Obtain the minimum and maximum ranges to use preprocessing.
-        min_range = rospy.get_param('/lidar2d/min_range')
-        max_range = rospy.get_param('/lidar2d/max_range')
+        min_range = rospy.get_param('/lidar3d/min_range')
+        max_range = rospy.get_param('/lidar3d/max_range')
 
-        # Preprocess the ranges into a 2D LIDAR scan.
-        scan_b = self.preprocess_measurement(
+        # Preprocess the ranges into a 3D LIDAR cloud.
+        cloud_b = self.preprocess_measurement(
             msg,
             min_range=min_range,
             max_range=max_range,
         )
 
         # Ignore if is the first received measurement.
-        if len(self.submap_scans) == 0:
-            self.submap_scans.append(scan_b)
+        if len(self.submap_clouds) == 0:
+            self.submap_clouds.append(cloud_b)
             return
-        scan_a = self.submap_scans[-1]
+        cloud_a = self.submap_clouds[-1]
 
         # Create the LIDAR factor, add to the optimizer, and optimize.
         factor, wTb_estimate = self.create_lidar_factor(
             index_a,
             index_b,
-            scan_a,
-            scan_b,
+            cloud_a,
+            cloud_b,
             aTb_estimate
         )
         self.optimizer.add_factor(factor, (X(index_b), wTb_estimate))
         self.optimizer.optimize()
-        self.submap_scans.append(scan_b)
+        self.submap_clouds.append(cloud_b)
 
         # Create skip connections to create a denser graph.
-        with self.lidar2d_lock:
+        with self.lidar3d_lock:
             current_state = self.state_index
-            submap = self.submap_scans.copy()
+            submap = self.submap_clouds.copy()
         if len(submap) > 2:
             self.create_skip_connections(submap, current_state)
             self.optimizer.optimize()
-        if len(submap) == rospy.get_param('/lidar2d/submap_length'):
-            self.publish_transformed_scan(
+        if len(submap) == rospy.get_param('/lidar3d/submap_length'):
+            self.publish_transformed_cloud(
                 submap[0],
                 current_state - len(submap) + 1
             )
@@ -187,11 +177,10 @@ class Lidar2DWrapper:
         Create skip connections between non-consecutive poses.
 
         Args:
-            submap:        A list of the most recently recorded scans, 
-                            where the last scan is the current scan.
-            current_index: The state index associated with the current scan.
+            submap:        A list of the most recently recorded clouds, 
+                            where the last cloud is the current cloud.
+            current_index: The state index associated with the current cloud.
         """
-        start = time.time()
         submap_length = len(submap)
         for i in range(submap_length - 2):
             index_a = current_index - i - 2
@@ -201,75 +190,69 @@ class Lidar2DWrapper:
                 index_a,
                 index_b
             )
-        end = time.time()
-        print("-"*15)
-        print(f"total of {submap_length} scans: {end - start} s")
-        print(f"average for each connection: {(end - start) / submap_length} s")
-        # map(self.create_skip_connection, )
 
-    def create_skip_connection(self, scans, index_a, index_b):
+    def create_skip_connection(self, clouds, index_a, index_b):
         """
         Create a particular skip connection between two poses.
 
         Args:
-            scans:   A list of the most recently recorded scans,
-                     where the last scan is the current scan.
+            clouds:   A list of the most recently recorded clouds,
+                     where the last cloud is the current cloud.
             index_a: The index associated with a previous time step.
             index_b: The index associated with the current time step.
         """
-        scan_a, scan_b = scans[-(index_b - index_a + 1)], scans[-1]
+        cloud_a, cloud_b = clouds[-(index_b - index_a + 1)], clouds[-1]
         factor, _ = self.create_lidar_factor(
             index_a,
             index_b,
-            scan_a,
-            scan_b
+            cloud_a,
+            cloud_b
         )
         self.optimizer.add_factor(factor)
 
-    def publish_transformed_scan(self, bTscan, index):
-        """Transform a scan from the body frame into the world frame.
+    def publish_transformed_cloud(self, bTcloud, index):
+        """Transform a cloud from the body frame into the world frame.
 
         Args:
-            bTscan: The scan, currently in the body frame.
-            index:  The state index of the to-be transformed scan.
+            bTcloud: The cloud, currently in the body frame.
+            index:  The state index of the to-be transformed cloud.
         """
 
         # Obtain the optimized pose.
         wTb = self.optimizer.results.atPose3(X(index))
 
-        # Transform the scan from the body frame to the world frame.
-        wTscan = wTb.transformFrom(
-            np.vstack((bTscan, np.zeros((1, bTscan.shape[1])))))
+        # Transform the cloud from the body frame to the world frame.
+        wTcloud = wTb.transformFrom(bTcloud)
 
-        # Reformat scan to be argument for creating point cloud.
-        wTscan = wTscan.T
+        # Reformat cloud to be argument for creating point cloud.
+        wTcloud = wTcloud.T
         header = Header()
         header.stamp = rospy.Time.now()
         header.frame_id = "map"
-        pcd = point_cloud2.create_cloud_xyz32(header, wTscan)
-        self.scan_publisher.publish(pcd)
+        pcd = point_cloud2.create_cloud_xyz32(header, wTcloud)
+        self.cloud_publisher.publish(pcd)
 
     # Instantiating parameters
 
     def initialize_params(self):
         """Initialize LIDAR parameters based on the set rosparams."""
 
-        # Instantiate 2D LIDAR related attributes.
-        self.submap_scans = deque(
-            [], rospy.get_param('/lidar2d/submap_length'))
+        # Instantiate 3D LIDAR related attributes.
+        self.submap_clouds = deque(
+            [], rospy.get_param('/lidar3d/submap_length'))
 
         # Parse the noise standard deviation parameters and create a GTSAM noise model.
-        icp_noise_sigmas = rospy.get_param('/lidar2d/icp_noise')
+        icp_noise_sigmas = rospy.get_param('/lidar3d/icp_noise')
         self.icp_noise_model = gtsam.noiseModel.Diagonal.Sigmas(
             np.array([
-                1e-4,
-                1e-4,
+                np.deg2rad(icp_noise_sigmas[0]),
+                np.deg2rad(icp_noise_sigmas[1]),
                 np.deg2rad(icp_noise_sigmas[2]),
                 icp_noise_sigmas[0],
                 icp_noise_sigmas[1],
-                1e-3
+                icp_noise_sigmas[2]
             ])
         )
 
         self.correspondence_threshold = rospy.get_param(
-            '/lidar2d/correspondence_threshold')
+            '/lidar3d/correspondence_threshold')
